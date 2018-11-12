d45039a6c9daeb973755ff464a076d6e7dd713a0
Francisco Javier Hidalgo Frias <friasjavi45@gmail.com>
2018-11-12 14:01:39 +0100

    Fragment10 - Paso3(finalizado)

--- app/src/main/java/com/iesvirgendelcarmen/dam/fragment10/Fragment10.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment10/Fragment10.java
@@ -8,17 +8,29 @@ import android.widget.Toast;
 
 public class Fragment10 extends FragmentActivity implements Fragmento1.CallBacks {
 
+    private boolean dosFragmentos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment10);
+        if(findViewById(R.id.frame_contenedor) != null){
+            dosFragmentos = true;
+        };
     }
 
     @Override
     public void onEntradaSeleccionada(String id) {
-        //Toast.makeText(getBaseContext(),"TOCADO EL " + id, Toast.LENGTH_SHORT).show();
-        Intent detalleIntent = new Intent(this, Fragmento2.class);
-        detalleIntent.putExtra(Fragmento3.ARG_ID_ENTRADA_SELECCIONADA, id);
-        startActivity(detalleIntent);
+        if(dosFragmentos){
+            Bundle arguments = new Bundle();
+            arguments.putString(Fragmento3.ARG_ID_ENTRADA_SELECCIONADA, id);
+            Fragmento3 fragment = new Fragmento3();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_contenedor, fragment).commit();
+        } else {
+            //Toast.makeText(getBaseContext(),"TOCADO EL " + id, Toast.LENGTH_SHORT).show();
+            Intent detalleIntent = new Intent(this, Fragmento2.class);
+            detalleIntent.putExtra(Fragmento3.ARG_ID_ENTRADA_SELECCIONADA, id);
+            startActivity(detalleIntent);
+        }
     }
 }
